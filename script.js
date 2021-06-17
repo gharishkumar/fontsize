cf0fc432bdf4ce7bc06cd3b527e5af8f9fc1894d
Harish Kumar <g.harishkumar1998@gmail.com>
2021-06-17 18:51:30 +0530

    + size adjustment

---
+++ script.js
@@ -0,0 +1,52 @@
+
+window.addEventListener("load",  function afterload(){
+    var contents = document.getElementsByClassName('content');
+    const size = [1,8/13,6/13,7/26,5/26,2/13,3/26,1/13,1/26];
+
+    contents[0].style.fontSize = '20px';
+
+    for(var i=1; i < contents.length; i++) {
+        contents[i].style.fontSize = ((contents[0].style.fontSize.slice(0, -2) - 0) *size[i] ) + 'px';
+    }
+    document.onkeydown = checkKey;
+    function checkKey(e) {
+        e = e || window.event;
+        if (e.keyCode == '38') {
+            // up arrow
+            contents[0].style.fontSize = ((contents[0].style.fontSize.slice(0, -2) - 0) + 10 ) + 'px';
+
+            for(var i=1; i < contents.length; i++) {
+                contents[i].style.fontSize = ((contents[0].style.fontSize.slice(0, -2) - 0) *size[i] ) + 'px';
+            }
+            console.log(contents[0].style.fontSize);
+        }
+        else if (e.keyCode == '40') {
+            // down arrow  
+            contents[0].style.fontSize = (contents[0].style.fontSize.slice(0, -2) - 10 ) + 'px';
+
+            for(var i=1; i < contents.length; i++) {
+                contents[i].style.fontSize = ((contents[0].style.fontSize.slice(0, -2) - 0) *size[i] ) + 'px';
+            }
+            console.log(contents[0].style.fontSize);
+        }
+        else if (e.keyCode == '37') {
+            // left arrow
+            contents[0].style.fontSize = (contents[0].style.fontSize.slice(0, -2) - 1 ) + 'px';
+
+            for(var i=1; i < contents.length; i++) {
+                contents[i].style.fontSize = ((contents[0].style.fontSize.slice(0, -2) - 0) *size[i] ) + 'px';
+            }
+            console.log(contents[0].style.fontSize);
+        }
+        else if (e.keyCode == '39') {
+            // right arrow
+            contents[0].style.fontSize = ((contents[0].style.fontSize.slice(0, -2) - 0) + 1 ) + 'px';
+
+            for(var i=1; i < contents.length; i++) {
+                contents[i].style.fontSize = ((contents[0].style.fontSize.slice(0, -2) - 0) *size[i] ) + 'px';
+            }
+            console.log(contents[0].style.fontSize);
+        }
+
+}
+});
